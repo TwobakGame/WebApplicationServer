12490a04395ef13e006e4fc9c4b37f5d760fafaf
gns0314 <gns0314@naver.com>
2024-01-13 17:21:14 +0900

    ✨ : JWT를 이용한 user crud 기능 구현

---
+++ users/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import SigninView, MyTokenObtainPairView, LogoutView, ChangePwView, ProfileUpdateView, DeleteUserView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+app_name = 'user'
+
+urlpatterns = [
+    # 회원가입
+    path('signin/', SigninView.as_view(), name='signin'),
+    # 로그인
+    path('login/',  MyTokenObtainPairView.as_view(), name='login'),
+    # 토큰 재발급
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # 로그아웃
+    path('logout/',  LogoutView.as_view(), name='logout'),
+    # 비밀번호 변경
+    path('changepw/', ChangePwView.as_view(), name='changepw'),
+    # 회원정보 수정
+    path('profileupdate/', ProfileUpdateView.as_view(), name='profileupdate'),
+    # 회원 탈퇴
+    path('deleteuser/', DeleteUserView.as_view(), name='deleteuser'),
+    
+]
---
+++ users/views.py
@@ -0,0 +1,144 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import SigninSerializer, ChangePasswordSerializer, ProfileUpdateSerializer, DeleteUserSerializer
+
+from .models import RefreshToken 
+
+
+# 회원가입
+class SigninView(APIView):
+    def post(self, request):
+        serializer = SigninSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': '회원가입 성공'}, status=status.HTTP_201_CREATED)
+        
+        return Response({'error_code': status.HTTP_400_BAD_REQUEST},status= status.HTTP_400_BAD_REQUEST)
+    
+
+# 로그인 성공시 토큰 발급
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['email'] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        refresh_token, _ = RefreshToken.objects.get_or_create(user=self.user)
+        refresh_token.token = str(refresh)
+        refresh_token.save()
+        return {'RefreshToken': data['refresh'],'AccessToken': data['access']}
+
+
+# 로그인
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+# 로그아웃
+class LogoutView(APIView):
+    permissions_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.refresh_token.delete()
+
+        return Response({'message': '로그아웃 성공'}, status=status.HTTP_200_OK)
+    
+
+# 비밀번호 변경
+class ChangePwView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = request.user
+            current_password = serializer.validated_data.get('current_password')
+            new_password = serializer.validated_data.get('new_password')
+
+            if user.check_password(current_password):
+                user.set_password(new_password)
+                user.save()
+                return Response({'message': '비밀번호 변경 성공','status':200 },status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'error_code': status.HTTP_400_BAD_REQUEST,
+                    'error': '현재 암호가 틀립니다.'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+            
+        return Response({
+                        'error_code': status.HTTP_400_BAD_REQUEST,
+                        'error': serializer.errors['new_password']
+                        }, 
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+
+# 회원 정보 수정
+class ProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = ProfileUpdateSerializer(user)
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        user = request.user
+        serializer = ProfileUpdateSerializer(user, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({
+            'error_code': status.HTTP_400_BAD_REQUEST,
+            'error': serializer.errors
+            }, 
+            status=status.HTTP_400_BAD_REQUEST)
+
+
+# 회원탈퇴
+class DeleteUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        serializer = DeleteUserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            password = serializer.validated_data.get('password')
+
+            if user.check_password(password):
+                try:
+                    RefreshToken.objects.get(user_id=user.id).delete()
+                    user.is_active = False
+                    user.save()
+                    return Response({'message':'회원 탈퇴 성공','status':status.HTTP_200_OK}, status=status.HTTP_200_OK)
+                except RefreshToken.DoesNotExist:
+                    return Response({
+                        'error_code': status.HTTP_404_NOT_FOUND,
+                        'error': '유효하지 않는 유저정보 입니다.'
+                        },
+                        status=status.HTTP_404_NOT_FOUND)
+            
+            return Response({
+                'error_code': status.HTTP_401_UNAUTHORIZED,
+                'error': '유효하지 않는 유저정보 입니다.'
+                },
+                status=status.HTTP_401_UNAUTHORIZED)
